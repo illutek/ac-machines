b3990fa51d67ce0067db478d3ddcf9580435f3c6
Vandenborne Stefan <stefan@illutek.be>
2014-10-08 22:49:36 +0200

    #

---
+++ templates/html.tpl.php
@@ -0,0 +1,25 @@
+<?php // $Id$   ?>     
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"       "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">     
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>         
+ <head profile="<?php print $grddl_profile; ?>">       
+<?php print $head; ?> 
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" /> 
+<meta name="description" content="Ac Machines is gespecialiseerd in aan – verkoop van minitractoren, tuinbouwmachines, kniktractoren van de merkenKubota, Iseki, Yanmar, Valpadana, Holder, Hinomoto, Goldoni, Shibaura, Mitsubishi" />     
+<title><?php print $head_title; ?></title>       
+<?php print $styles; ?>       
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"> </ script>
+      	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->      
+        
+</head>   
+<body class="<?php print $classes; ?>" <?php print $attributes; ?>>        
+<?php print $page_top; ?>       
+<?php print $page; ?>       
+<?php print $page_bottom; ?>  
+<?php print $scripts; ?>
+</body> 
+</html>
